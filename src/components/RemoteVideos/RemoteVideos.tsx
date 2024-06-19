3c4f6e56c4f534a38e376335262c262b331d56ed
Gokula Kannan L <64990358+Gokula-Kannan-L@users.noreply.github.com>
2024-06-19 18:04:36 +0530

    Update RemoteVideos.tsx

--- src/components/RemoteVideos/RemoteVideos.tsx
+++ src/components/RemoteVideos/RemoteVideos.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from "react";
-import { RemoteVideo, useAttendeeStatus } from 'amazon-chime-sdk-component-library-react';
-
+import React, { FunctionComponent, useEffect } from "react";
+import { RemoteVideo, useAttendeeStatus, useRemoteVideoTileState } from 'amazon-chime-sdk-component-library-react';
+import Avatar from '@mui/material/Avatar';
 
 type props = {
     AttendeeId: string
@@ -15,10 +15,19 @@ const RemoteVideos:FunctionComponent<props> = ({AttendeeId}) => {
         signalStrength
       } = useAttendeeStatus(AttendeeId);
 
+    const { tiles, attendeeIdToTileId } = useRemoteVideoTileState();
+
+    useEffect( () => {
+        console.log(AttendeeId, attendeeIdToTileId[AttendeeId]);
+        
+    },[tiles, attendeeIdToTileId])
     return(
         <>
         {
-            videoEnabled ? <RemoteVideo tileId={2}/> : <>hello</>
+            videoEnabled ? 
+                <RemoteVideo tileId={attendeeIdToTileId[AttendeeId]} name={AttendeeId}/> 
+                : 
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}><Avatar sx={{ width: 100, height: 100, fontSize: 24, fontWeight:600 }}>hi</Avatar></div>
         }
         </>
     )
