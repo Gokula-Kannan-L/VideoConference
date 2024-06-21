070f28a2ff580cfe8e471b8d388e5b878b8a3cbd
Gokula Kannan L <64990358+Gokula-Kannan-L@users.noreply.github.com>
2024-06-21 16:33:43 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalProvider } from './GlobalProvider/GlobalContext';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GlobalProvider>
   </React.StrictMode>
 );
 
